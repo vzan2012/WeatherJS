10e00b122bd1f7a2d3062ca6ea46469bf6cbaa46
Deepak Guptha <vzan2012@gmail.com>
2019-06-09 22:12:51 +0530

    feat(scripts): Fetching the Weather API

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
  * @ Author: Sitharaman Deepak Guptha
  * @ Description: App Scripts
  */
+
+//  Initiate the Weather Object
+const weather = new Weather("Paris", "France");
+
+// Change Location - Function
+// weather.changeLocation('Chennai', 'India');
+
+function getWeather() {
+  weather
+    .getWeather()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
--- js/weather.js
+++ js/weather.js
@@ -2,3 +2,29 @@
  * @ Author: Sitharaman Deepak Guptha
  * @ Description: Weather Scripts
  */
+class Weather {
+  constructor(city, country) {
+    this.apikey = "";
+    this.city = city;
+    this.country = country;
+  }
+
+  // Fetch Weather from the API
+  async getWeather() {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${
+        this.country
+      }&appid=${this.apikey}`
+    );
+
+    const responseData = await response.json();
+
+    return responseData;
+  }
+
+  // Change the Weather Location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
